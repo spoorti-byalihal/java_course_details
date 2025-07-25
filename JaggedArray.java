9d158fcbfcbb8baa97bfddcfc9527972b1451176
spoorti-byalihal <spoortid00@gmail.com>
2025-07-25 16:44:33 +0530

    Update JaggedArray.java

--- JaggedArray.java
+++ JaggedArray.java
@@ -3,7 +3,7 @@ public class JaggedArray {
         int arr[][]=new int[3][];
         arr[0]=new int[3];
         arr[1]=new int[4];
-        arr[2]=new int[3];
+        arr[2]=new int[5];
 
         for(int i=0;i<arr.length;i++){
             for (int j=0;j<arr[i].length;j++){
